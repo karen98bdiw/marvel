4ecdb05d7fbec3aff9e4465e92406a509709738d
karen98bdiw <ghazaryan.deb@mail.ru>
2021-05-07 11:00:31 +0400

    api

--- lib/api/api_constats.dart
+++ lib/api/api_constats.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+
 class ApiConstats {
   static const BaseUrl = "https://gateway.marvel.com";
 
   static const Public_Key = "035aab9a4c2d0fcdb71f0539db242bfa";
   static const Private_Key = "0db57f362aed8dd287f1662051604aaa8b3068f1";
-  static const TS = DateTime.now().millisecondsSinceEpoch
+  static final ts = DateTime.now().millisecondsSinceEpoch.toString();
+  static final hash =
+      md5.convert(utf8.encode("$ts$Private_Key$Public_Key")).toString();
 
   static const All_Character_GEt = "/v1/public/characters";
 }
---
+++ lib/api/characters_services.dart
@@ -0,0 +1,7 @@
+import 'package:dio/dio.dart';
+
+class CharachtersServices {
+  final Dio dio;
+
+  CharachtersServices({this.dio});
+}
