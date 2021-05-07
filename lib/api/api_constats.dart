3396b29cbc0fc3a71845958ee5d62e1a165a463f
karen98bdiw <ghazaryan.deb@mail.ru>
2021-05-07 10:52:33 +0400

    structure

---
+++ lib/api/api_constats.dart
@@ -0,0 +1,9 @@
+class ApiConstats {
+  static const BaseUrl = "https://gateway.marvel.com";
+
+  static const Public_Key = "035aab9a4c2d0fcdb71f0539db242bfa";
+  static const Private_Key = "0db57f362aed8dd287f1662051604aaa8b3068f1";
+  static const TS = DateTime.now().millisecondsSinceEpoch
+
+  static const All_Character_GEt = "/v1/public/characters";
+}
